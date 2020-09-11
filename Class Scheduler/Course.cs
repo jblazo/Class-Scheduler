1744230be7966e88392944d5fdb6d56f204a2e3c
jblazo <jarodblazo@gmail.com>
2020-09-11 09:07:01 -0400

    Created Course class

---
+++ Class Scheduler/Course.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Class_Scheduler
+{
+    [Serializable]
+    class Course
+    {
+        private string courseDescription;
+        private string courseCode;
+        private string[] prerequisites;
+
+        public Course(string courseCode, string courseDescription, string[] preReqs)
+        {
+            this.courseCode = courseCode;
+            this.courseDescription = courseDescription;
+            this.prerequisites = preReqs;
+        }
+
+        public string CourseCode
+        {
+            get
+            {
+                return this.courseCode;
+            }
+            set
+            {
+                this.courseCode = value;
+            }
+        }
+
+        public string CourseDescription
+        {
+            get
+            {
+                return this.courseDescription;
+            }
+            set
+            {
+                this.courseDescription = value;
+            }
+        }
+
+        public string[] PreRequisites
+        {
+            get
+            {
+                return this.prerequisites;
+            }
+            set
+            {
+                this.prerequisites = value;
+            }
+        }
+    }
+}
